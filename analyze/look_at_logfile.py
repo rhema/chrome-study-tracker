5ffca9627e558f2faf04748dcf10964cb54122b5
Rhema Linder <rhemalinder@gmail.com>
2012-10-25 12:01:29 -0500

    Added initial visualization using d3 for tail of logs.

--- analyze/look_at_logfile.py
+++ analyze/look_at_logfile.py
@@ -1,3 +1,5 @@
+#longest with anonyuser10b
+
 #metrics we might want to look at:
 #fluency on impression / collection
 #percentage of PDF views
@@ -10,11 +12,14 @@
 #total number of expands
 #total viewing time...
 
+''' '''
+
 import json
 print "testing"
 
 #input_file = open("browser-log.txt")
 input_file = open("aggregatelog.txt")
+#input_file = open("nony10.txt")
 input_lines = input_file.readlines()
 print "Input has",str(len(input_lines)),"entries"
 
@@ -73,4 +78,124 @@ for user in users_in_order:
 #            print tab_event
         total_tab_time += seconds
     print "Total tab time:",total_tab_time/60.0
+
+
+def expand_or_crumb_to_dend(item):
+    item = item['item']
+    dend = {}
+    source = None
+    try:
+        source = item['source']['title']
+    except:
+        return None
+    
+    if 'parents' in item:
+        current_children = None
+    if len(item['parents']) == 0:
+        print "breaking"
+        return {'name':source}
+    parents = item['parents']
+    print "Dos parents",parents,len(parents)
+    if parents > 1:
+        parents = parents.reverse()
+    for parent in item['parents']:
+        if current_children is None:
+            current_children = []
+            dend['name'] = parent['title']
+            dend['children'] = current_children
+        else:
+            new_children = []
+            current_children.append({'name':parent['title'],'children':new_children})
+            current_children = new_children
+    current_children.append({'name':source})        
+    #for child in item['item']['parents']:#maybe need to reverse....
+    #    print parent
+    return dend
+
+def long_dend_length(dend):
+    length = 1
+    children = None
+    if "children" in dend:
+        children = dend['children']
+    else:
+        return length
+    while children is not None:
+        length += 1
+        if "children" in children[0]:
+            children = children[0]['children']
+        else:
+            break
+    return length
+
+
+total_extension_max = 0
+
+print "TRY OUT MAKING AN OUTPUT FILE....MAN"
+outfile = open("output/data/dend_data.js","w")
+
+for user in users_in_order:
+    total_events = 0
+    sum_of_length = 0
+    print "den for user",user
+    title_to_dend = {}
+    all_dends = []
+    u_children = []
+    ultimate = {'name':'root','children':u_children}
+    for event in by_user[user]['incontext_expand_crumb']:
+#        dend = json.dumps(expand_or_crumb_to_dend(event),indent = 4)
+        dend = expand_or_crumb_to_dend(event)
+        if dend is None:
+            continue
+        longness = long_dend_length(dend)
+        print dend['name'],"->",longness
+        if total_extension_max < longness:
+            total_extension_max = longness
+#goal here is to follow chain of children until not found starting with source... title should always be added if not exists already in root.
+        #make sure root doc exists in root
+        if not dend['name'] in title_to_dend:
+            print "Adding to root:",dend['name']
+            start_page_dend = {'name': dend['name'], 'children':[]}
+            title_to_dend[dend['name']] = start_page_dend
+            u_children.append(start_page_dend)
+        print "Attempt to print all children in order:"
+        children = None
+        parent = None
+        if "children" in dend:
+            children = dend['children']
+            parent = dend['name']
+        else:
+            continue
+        while children is not None:
+            if "children" in children[0]:
+                #parent, here must be indexed
+                #if I already exists, skip adding part
+                ##adding part
+                if not children[0]['name'] in title_to_dend:
+                    parent_dend = title_to_dend[parent]
+                    print "pre existing parent",parent_dend
+                    print children[0]['name']
+                    child_dend = {'name': children[0]['name'], 'children': []}
+                    title_to_dend[children[0]['name']] = child_dend
+                    parent_dend['children'].append(child_dend)
+                parent = children[0]['name']
+                children = children[0]['children']
+            else:
+                break
+        
+        #print all children
+        
+        #we now have a guarentee that the root doc will exist... next, iterate over all children, finding the right spot to add self
+#        if dend['name'] in title_to_dend:
+#            if not 'children' in title_to_dend:
+#                title_to_dend['children'] = []
+#            if 'children' in dend:
+#                print "extending children..."
+#                title_to_dend['children'].extend(dend['children'])#need to check each child as well...
+#        else:
+#            title_to_dend[dend['name']] = dend
+#            u_children.append(dend)
+#user_dends.push({name: "meh", value: {boop:"beep"}});
+    outfile.write("user_dends.push({name: '"+user+"', value: "+json.dumps(ultimate,indent = 4)+"});\n\n\n")
+        #always add, but where?
 #print by_user
+print "longest",total_extension_max
---
+++ analyze/output/default2.js
@@ -0,0 +1,130 @@
+var json_in = {
+    "name": "root", 
+    "children": [
+        {
+            "name": "Thumbs up?: sentiment classification using machine learning techniques", 
+            "children": []
+        }, 
+        {
+            "name": "Mining Query Logs: Turning Search Usage Data into Knowledge", 
+            "children": []
+        }, 
+        {
+            "name": "The power of naive query segmentation", 
+            "children": []
+        }, 
+        {
+            "name": "Suggesting related topics in web search", 
+            "children": [
+                {
+                    "name": "Mining search engine query logs for query recommendation", 
+                    "children": [
+                        {
+                            "name": "A unified representation of web logs for mining applications", 
+                            "children": []
+                        }, 
+                        {
+                            "name": "The query-flow graph: model and applications", 
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "name": "Towards query log based personalization using topic models", 
+            "children": [
+                {
+                    "name": "To each his own: personalized content selection based on text comprehensibility", 
+                    "children": [
+                        {
+                            "name": "Large-scale learning of word relatedness with constraints", 
+                            "children": [
+                                {
+                                    "name": "WikiWalk: random walks on Wikipedia for semantic relatedness", 
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }, 
+        {
+            "name": "Effective anonymization of query logs", 
+            "children": [
+                {
+                    "name": "User k-anonymity for privacy preserving data mining of query logs", 
+                    "children": []
+                }, 
+                {
+                    "name": "Differentially private search log sanitization with optimal output utility", 
+                    "children": [
+                        {
+                            "name": "Frequent pattern mining: current status and future directions", 
+                            "children": [
+                                {
+                                    "name": "Mining GPS traces and visual words for event classification", 
+                                    "children": []
+                                }, 
+                                {
+                                    "name": "Mining top-k regular-frequent itemsets using database partitioning and support estimation", 
+                                    "children": []
+                                }, 
+                                {
+                                    "name": "Sweeping the disjunctive search space towards mining new exact concise representations of frequent itemsets", 
+                                    "children": [
+                                        {
+                                            "name": "Generalization of association rules through disjunction", 
+                                            "children": [
+                                                {
+                                                    "name": "Closures of Downward Closed Representations of Frequent Patterns", 
+                                                    "children": []
+                                                }
+                                            ]
+                                        }, 
+                                        {
+                                            "name": "Mining concise representations of frequent patterns through conjunctive and disjunctive search spaces", 
+                                            "children": [
+                                                {
+                                                    "name": "Frequent closed itemset based algorithms: a thorough structural and analytical survey", 
+                                                    "children": [
+                                                        {
+                                                            "name": "Mining frequent itemsets from multidimensional databases", 
+                                                            "children": [
+                                                                {
+                                                                    "name": "A Novel Classification Algorithm Based on Association Rules Mining", 
+                                                                    "children": []
+                                                                }, 
+                                                                {
+                                                                    "name": "Mining association rules between sets of items in large databases", 
+                                                                    "children": []
+                                                                }, 
+                                                                {
+                                                                    "name": "CMAR: Accurate and Efficient Classification Based on Multiple Class-Association Rules", 
+                                                                    "children": []
+                                                                }, 
+                                                                {
+                                                                    "name": "Mining minimal non-redundant association rules using frequent itemsets lattice", 
+                                                                    "children": []
+                                                                }, 
+                                                                {
+                                                                    "name": "Efficient Algorithms for Mining Closed Itemsets and Their Lattice Structure", 
+                                                                    "children": []
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
